3c66cbf55dfd9f74573bdf66638529d69dcea9be
Usman Akbar <45451919+usmanjoiya@users.noreply.github.com>
2025-12-11 21:49:08 +0500

    updated field

--- measurement/models/inh_mrp_bom.py
+++ measurement/models/inh_mrp_bom.py
@@ -3,7 +3,7 @@ from odoo import _, models, fields, api
 class MrpBom(models.Model):
     _inherit = 'mrp.bom'
 
-    total_sale_price = fields.Float(string="Total Sale Price", compute="_compute_total_sale_price", store=True)
+    total_sale_price = fields.Float(string="Total Sale Price", compute="_compute_total_sale_price")
 
     @api.depends('bom_line_ids', 'bom_line_ids.product_id', 'bom_line_ids.product_qty')
     def _compute_total_sale_price(self):
